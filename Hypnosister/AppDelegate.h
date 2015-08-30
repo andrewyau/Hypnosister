dca5c692de9164d7f92cdd2a97ecc8522ef36073
Andrew Yau <andrewyau@Andrews-MacBook.local>
2015-08-30 23:00:14 +0100

    Initial Commit

---
+++ Hypnosister/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Hypnosister
+//
+//  Created by Andrew Yau on 30/08/2015.
+//  Copyright (c) 2015 Andrew Yau. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Hypnosister/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Hypnosister
+//
+//  Created by Andrew Yau on 30/08/2015.
+//  Copyright (c) 2015 Andrew Yau. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
